cc9ba3ea02d84e9d16520af9ae4dd1e365147f38
Daniel Tipping <daniel@oldreliable.tech>
2019-12-08 10:02:31 +0000

    Remove metview support
    The feedstock magics-metview contains magics built with metview enabled.
    This way the magics feedstock is smaller and less complicated.

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-export PYTHON="$PYTHON"
 export PYTHON_LDFLAGS="$PREFIX/lib"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
 export CFLAGS="$CFLAGS -fPIC -I$PREFIX/include"
@@ -10,11 +9,7 @@ export CFLAGS="$CFLAGS -fPIC -I$PREFIX/include"
 export REPLACE_TPL_ABSOLUTE_PATHS=0
 if [[ $(uname) == Linux ]]; then
   export REPLACE_TPL_ABSOLUTE_PATHS=1
-  # workaround for https://github.com/conda-forge/qt-feedstock/issues/123
-  sed -i 's|_qt5gui_find_extra_libs(EGL.*)|_qt5gui_find_extra_libs(EGL "EGL" "" "")|g' $PREFIX/lib/cmake/Qt5Gui/Qt5GuiConfigExtras.cmake
-  sed -i 's|_qt5gui_find_extra_libs(OPENGL.*)|_qt5gui_find_extra_libs(OPENGL "GL" "" "")|g' $PREFIX/lib/cmake/Qt5Gui/Qt5GuiConfigExtras.cmake
 fi
-
 
 mkdir ../build && cd ../build
 
@@ -26,12 +21,6 @@ cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       $SRC_DIR
 
 make -j $CPU_COUNT
-
-if [[ $(uname) == Linux ]]; then
-    # Tell Linux where to find libGL.so.1 and other libs needed for Qt
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$BUILD_PREFIX/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib64/
-fi
-
 ctest --output-on-failure -j $CPU_COUNT
 make install
 
@@ -39,4 +28,8 @@ make install
 ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
 DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
 cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/magics-activate.sh
+cp $RECIPE_DIR/scripts/activate.fish $ACTIVATE_DIR/magics-activate.fish
+cp $RECIPE_DIR/scripts/activate.csh $ACTIVATE_DIR/magics-activate.csh
 cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/magics-deactivate.sh
+cp $RECIPE_DIR/scripts/deactivate.fish $DEACTIVATE_DIR/magics-deactivate.fish
+cp $RECIPE_DIR/scripts/deactivate.csh $DEACTIVATE_DIR/magics-deactivate.csh
